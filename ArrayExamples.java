3b3c97b1bad279564d6ef65c2f7f04cb7422d116
Paz Mogrelia <paz.mogrelia@gmail.com>
2024-02-01 12:52:13 -0800

    Fixed average without lowest and added test cases
    Fixed function error with low number of items & repeated lowest items.

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -25,14 +25,14 @@ public class ArrayExamples {
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
   static double averageWithoutLowest(double[] arr) {
-    if(arr.length < 2) { return 0.0; }
-    double lowest = arr[0];
-    for(double num: arr) {
-      if(num < lowest) { lowest = num; }
+    if(arr.length <= 1) { return 0.0; }
+    int lowestIndex = 0;
+    for(int i = 0; i<arr.length;i++) {
+      if(arr[i] < arr[lowestIndex]) { lowestIndex = i; }
     }
     double sum = 0;
-    for(double num: arr) {
-      if(num != lowest) { sum += num; }
+    for(int i = 0; i<arr.length;i++) {
+      if(i != lowestIndex) { sum += arr[i]; }
     }
     return sum / (arr.length - 1);
   }
--- ArrayTests.java
+++ ArrayTests.java
@@ -28,4 +28,31 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 4,3,2,1}, input1);
   }
+
+  @Test
+  public void testAvgWithoutLowest(){
+    double input1[] = {4,1,2,9,3};
+    assertEquals(4.5,ArrayExamples.averageWithoutLowest(input1), 0.01);
+  }
+  @Test
+  public void testAvgWithoutLowestNegative(){
+    double input1[] = {4,9,-3};
+    assertEquals(6.5,ArrayExamples.averageWithoutLowest(input1), 0.01);
+  }
+  @Test
+  public void testAvgWithoutLowestHighValues(){
+    double input1[] = {1,100,100,150};
+    assertEquals(116.67,ArrayExamples.averageWithoutLowest(input1), 0.01);
+  }
+  @Test
+  public void testAvgWithoutLowestTwoItems(){
+    double input1[] = {2,2};
+    assertEquals(2,ArrayExamples.averageWithoutLowest(input1), 0.01);
+  }
+  @Test
+  public void testAvgWithoutLowestOneItem(){
+    double input1[] = {3};
+    assertEquals(0,ArrayExamples.averageWithoutLowest(input1), 0.01);
+  }
+
 }

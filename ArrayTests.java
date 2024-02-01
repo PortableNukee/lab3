a269b27ed9a540282c67dbc4bb1f132f502ca784
Paz Mogrelia <paz.mogrelia@gmail.com>
2024-02-01 12:36:59 -0800

    Fixed Reverse Functions and Tests

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,9 +4,9 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 3, 8, 9 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 9,8,3 }, input1);
 	}
 
 
@@ -14,5 +14,18 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseFourItems(){
+    int[] input1 = {4,424,2,7};
+    assertArrayEquals(new int[]{7,2,424,4}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlaceFourItems(){
+    int[] input1 = { 1,2,3,4 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 4,3,2,1}, input1);
   }
 }
